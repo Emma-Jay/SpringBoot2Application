744f67dc54d2d38d21b7b9fbb22486038d9bd317
Emma-Jay <iamhim.emmanuel@gmail.com>
2021-06-10 09:29:53 +0300

    Update SpringBoot2Application.java
    Reset to the original file

--- src/main/java/com/example/project/SpringBoot2Application.java
+++ src/main/java/com/example/project/SpringBoot2Application.java
@@ -1,22 +1,13 @@
 package com.example.project;
 
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+public class SpringBoot2Application {
 
-
-
-
-public class HospitalController {
-
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2Application.class, args);
+    }
 
 }
-
